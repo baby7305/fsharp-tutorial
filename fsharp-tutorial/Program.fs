c5aaa5c2498d4fc06f2aee90e4f73cd435f4c322
baby7305 <songhaibei7305@163.com>
2018-11-16 16:14:24 +0800

    vs中f#入门

--- fsharp-tutorial/Program.fs
+++ fsharp-tutorial/Program.fs
@@ -1,8 +1,8 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module HelloSquare
 
-open System
+let square x = x * x
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    printfn "%d squared is: %d!" 12 (square 12)
+    0 // Return an integer exit code
